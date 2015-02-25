8fc669b4b629cec3ce78adf503cdbd4aa3bbe830
siman-man <k128585@ie.u-ryukyu.ac.jp>
2015-02-25 20:23:43 +0900

    before boost

--- MovingNQueens.cpp
+++ MovingNQueens.cpp
@@ -3,14 +3,132 @@
 #include <cstdio>
 #include <map>
 #include <algorithm>
+#include <boost/multiprecision/cpp_int.hpp>
 #include <string.h>
 #include <sstream>
 #include <cassert>
 
 using namespace std;
+using namespace boost::multiprecision;
 
 const int MAX_QUEEN = 200;
 const int UNDEFINED = -1;
+
+class C128 {
+  private:
+    unsigned long long lo;
+    unsigned long long hi;
+
+  public:
+  C128(){
+    lo = 0;
+    hi = 0;
+  }
+  C128(unsigned long long m, unsigned long long n = 0){
+    lo = m;
+    hi = n; 
+  }
+  ~C128() {}
+
+  C128& operator<<=(unsigned int n){
+    n &= 0x07f;
+    if(n >= 64){
+      hi = lo << (n-64);
+      lo = 0;
+    }else if(n){
+      hi <<= n;
+      hi |= lo >> (64-n);
+      lo <<= n;
+    }
+
+    return *this;
+  }
+
+  C128& operator>>=(unsigned int n){
+    n &= 0x07f;
+    if(n >= 64){
+      lo = hi >> (n-64);
+      hi = 0;
+    }else if(n){
+      lo >>= n;
+      lo |= hi << (64-n);
+      hi >>= n;
+    }
+    return *this;
+  }
+
+  C128 operator<<(unsigned int n) const { 
+    return C128(*this) <<= n;
+  }
+
+  C128 operator>>(unsigned int n) const { 
+    return C128(*this) >>= n; 
+  }
+
+  C128& operator+=(unsigned long long u){
+    unsigned long long t = lo;
+    if((lo += u) < t){
+      ++hi;
+    }
+    return *this;
+  }
+
+  C128& operator+=(C128 c){
+    unsigned long long t = lo;
+    if((lo += c.lo) < t){
+      ++hi;
+    }
+    hi += c.hi;
+    return *this;
+  }
+
+  C128& operator-=(unsigned long long u){
+    unsigned long long t = lo;
+    if((lo -= u) > t){
+      --hi;
+    }
+    return *this;
+  }
+
+  C128 operator-=(C128 c){
+    unsigned long long t = lo;
+    if((lo -= c.lo) > t){
+      --hi;
+    }
+    hi -= c.hi;
+    return *this;
+  }
+
+  C128 operator&(C128 c) const { 
+    return C128(*this) &= c; 
+  }
+  C128 operator|(C128 c) const { return C128(*this) |= c; }
+  C128 operator^(C128 c) const { return C128(*this) ^= c; }
+  C128 operator~() const { return C128(~lo, ~hi); };
+  C128 operator+(unsigned long long u) const { return C128(*this) += u; }
+  C128 operator+(C128 c) const { return C128(*this) += c; }
+  C128 operator-(unsigned long long u) const { return C128(*this) -= u; }
+  C128 operator-(C128 c) const { return C128(*this) -= c; }
+  C128 operator-() const { return C128() -= *this; }
+
+  C128& operator&=(C128 c){
+    lo &= c.lo;
+    hi &= c.hi;
+    return *this;
+  }
+
+  C128& operator|=(C128 c){
+    lo |= c.lo;
+    hi |= c.hi;
+    return *this;
+  }
+
+  C128& operator^=(C128 c){
+    lo ^= c.lo;
+    hi ^= c.hi;
+    return *this;
+  }
+};
 
 struct Queen {
   int id;
@@ -39,6 +157,8 @@ class MovingNQueens {
     N = queenRows.size();
     int index, row, col;
     index = 0;
+    C128 c1(0);
+    C128 c2(2);
 
     for(int id = 0; id < N; ++id){
       row = queenRows[id];
@@ -122,6 +242,7 @@ int main(){
   vector<int> queenCols;
   vector<string> ret;
   MovingNQueens mq;
+  int128_t b;
 
   cin >> len;
   for(int i = 0; i < len; ++i){
